4b5f5bfa2876b07b23e3ea689cf675cd582a2072
lkladko <leonid.kladko@dataart.com>
2016-04-12 14:06:36 +0300

    bugfix

--- frontend/app/game/unit/archer.js
+++ frontend/app/game/unit/archer.js
@@ -23,16 +23,25 @@ define([
 
         move: function (point) {
             var self = this;
-            var target = new Square(point.y, point.x);
-            var steps = utils.getSteps(this.square, target);
-            var deltaX = (target.getXCoord() - this.square.getXCoord()) / steps;
-            var deltaY = (target.getYCoord() - this.square.getYCoord()) / steps;
-            var direction = utils.getDirection(deltaY, deltaX);
-            var moveAnimation = 'move' + direction;
-            var stayAnimation = 'stayArcher' + direction;
-            this.square = target;
+            var target = null;
+            var steps = 50;
+            var deltaX = null;
+            var deltaY = null;
+            var direction = null;
+            var moveAnimation = '';
+            var stayAnimation = '';
             return function () {
                 self.sprite.animations.play(moveAnimation, 8, true);
+                if (steps == 50) {
+                    target = new Square(point.y, point.x);
+                    steps = utils.getSteps(self.square, target);
+                    deltaX = (target.getXCoord() - self.square.getXCoord()) / steps;
+                    deltaY = (target.getYCoord() - self.square.getYCoord()) / steps;
+                    direction = utils.getDirection(deltaY, deltaX);
+                    moveAnimation = 'move' + direction;
+                    stayAnimation = 'stayArcher' + direction;
+                    self.square = target;
+                }
                 steps--;
                 if (steps == 0) {
                     self.sprite.animations.play(stayAnimation, 8, false);
@@ -46,17 +55,25 @@ define([
 
         attack: function (point) {
             var self = this;
-            var target = new Square(point.y, point.x);
+            var target = null;
             var steps = 70;
             var arrowSteps = 50;
-            var deltaX = (target.getXCoord() - this.square.getXCoord()) / arrowSteps;
-            var deltaY = (target.getYCoord() - this.square.getYCoord()) / arrowSteps;
-            var direction = utils.getDirection(deltaY, deltaX);
+            var deltaX = null;
+            var deltaY = null;
+            var direction = null;
             var shootAnimation = 'shoot' + direction;
             var stayAnimation = 'stayArcher' + direction;
             var arrow = null;
             return function () {
                 self.sprite.animations.play(shootAnimation, 8, false);
+                if (steps == 70) {
+                    target = new Square(point.y, point.x);
+                    deltaX = (target.getXCoord() - self.square.getXCoord()) / arrowSteps;
+                    deltaY = (target.getYCoord() - self.square.getYCoord()) / arrowSteps;
+                    direction = utils.getDirection(deltaY, deltaX);
+                    shootAnimation = 'shoot' + direction;
+                    stayAnimation = 'stayArcher' + direction;
+                }
                 steps--;
                 if (steps == 50) {
                     arrow = self.game.add.sprite(self.square.getXCoord(), self.square.getYCoord() + 32, 'arrow' + direction);
